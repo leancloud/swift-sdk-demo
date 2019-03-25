483beb9b76a435627cba08b5663fb82dc3bac13a
zapcannon87 <zapcannon87@outlook.com>
2019-03-25 18:49:31 +0800

    Init Chat

---
+++ swift-sdk-demo/Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-sdk-demo
+//
+//  Created by zapcannon87 on 2019/3/25.
+//  Copyright © 2019 LeanCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
